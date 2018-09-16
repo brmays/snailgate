386dc8992acc64a9832ed854359c5b6384f408bf
brmays <brudellmays@gmail.com>
2018-09-16 19:29:31 +0800

    First commit

---
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -0,0 +1,45 @@
+package mooc.vandy.java4android.gate.logic;
+
+/**
+ * This file defines the Gate class.
+ */
+public class Gate {
+    public final int in;
+    public final int out;
+    public final int closed;
+    private int mSwing;
+
+    public Gate() {
+        in = 1;
+        out = -1;
+        closed = 0;
+
+    }
+
+    public boolean (int direction) {
+
+
+    }
+
+    public boolean open(int direction) {
+
+    }
+
+    public void close() {
+
+
+    }
+
+    public int getSwingDirection() {
+
+    }
+
+    public int thru(int count) {
+
+    }
+
+    public String toString() {
+
+    }
+
+}
