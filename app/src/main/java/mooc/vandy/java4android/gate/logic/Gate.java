94d0c5c94208512ab39848adab8245c46eeae06f
Ben <brudellmays@gmail.com>
2018-09-17 15:35:11 +0800

    sep 17

--- app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
+++ app/src/main/java/mooc/vandy/java4android/gate/logic/Gate.java
@@ -4,42 +4,58 @@ package mooc.vandy.java4android.gate.logic;
  * This file defines the Gate class.
  */
 public class Gate {
-    public final int in;
-    public final int out;
-    public final int closed;
+    public final int IN;
+    public final int OUT;
+    public final int CLOSED;
     private int mSwing;
 
     public Gate() {
-        in = 1;
-        out = -1;
-        closed = 0;
-
+        IN = 1;
+        OUT = -1;
+        CLOSED = 0;
+        mSwing = CLOSED;
     }
 
-    public boolean (int direction) {
-
-
+    public boolean setSwing(int direction) {
+      boolean set = false;
+      if (direction == -1 || direction == 1) {
+        mSwing = direction;
+        set = true;
+      }
+      return set;
     }
 
     public boolean open(int direction) {
-
+      boolean opened = false;
+      if (direction == -1 || direction == 1) {
+        setSwing(direction);
+        opened = true;
+      }
+      return opened;
     }
 
     public void close() {
-
-
+      mSwing = 0;
     }
 
     public int getSwingDirection() {
-
+      return mSwing;
     }
 
     public int thru(int count) {
-
+        return (count * getSwingDirection());
     }
 
     public String toString() {
 
+      if (mSwing == CLOSED) { // a gate that is closed
+          return "This gate is closed";
+      } else if (mSwing == IN) { // a gate that has opened to swing IN
+          return "This gate is open and swings to enter the pen only";
+      } else if (mSwing == OUT) { // a gate that been opened swing OUT
+          return "This gate is open and swings to exit the pen only";
+      } else { // a gate that has a swing value other than IN, OUT, or CLOSED
+          return "This gate has an invalid swing direction";
+      }
     }
-
-}
+  }
